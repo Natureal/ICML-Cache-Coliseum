768a62f48a852b5e5e101964e83f1a10a16a011c
Natureal <naturechenpeng@gmail.com>
2026-06-05 16:29:21 +0800

    up

--- benchmark/__main__.py
+++ benchmark/__main__.py
@@ -313,6 +313,11 @@ if __name__ == "__main__":
                 PredictAlgorithmFactory.generate_predictive_algorithm(partial(PredictiveRPBOnlineMinContinuousAlgorithm, pred_budget=4, max_support_factor=100), 'PLECO'),
                 PredictAlgorithmFactory.generate_predictive_algorithm(partial(PredictiveRPBOnlineMinContinuousAlgorithm, pred_budget=8, max_support_factor=100), 'PLECO'),
                 PredictAlgorithmFactory.generate_predictive_algorithm(partial(PredictiveRPBOnlineMinContinuousAlgorithm, pred_budget=16, max_support_factor=100), 'PLECO'),
+                # HitCredit: persistent hit-accumulated credit (not reset on L0)
+                PredictAlgorithmFactory.generate_predictive_algorithm(partial(PredictiveRPBOnlineMinHitCreditAlgorithm, pred_budget=0, max_support_factor=100), 'PLECO'),
+                PredictAlgorithmFactory.generate_predictive_algorithm(partial(PredictiveRPBOnlineMinHitCreditAlgorithm, pred_budget=1, max_support_factor=100), 'PLECO'),
+                PredictAlgorithmFactory.generate_predictive_algorithm(partial(PredictiveRPBOnlineMinHitCreditAlgorithm, pred_budget=2, max_support_factor=100), 'PLECO'),
+                PredictAlgorithmFactory.generate_predictive_algorithm(partial(PredictiveRPBOnlineMinHitCreditAlgorithm, pred_budget=4, max_support_factor=100), 'PLECO'),
             ])
             combiner_types.extend([
                 (partial(CombineDeterministicAlgorithm, switch_bound=1, lazy_evictor_type=LRUEvictor), [PredictAlgorithmFactory.generate_predictive_algorithm(PredictAlgorithm, 'PLECO'), LRUAlgorithm]),
@@ -353,6 +358,11 @@ if __name__ == "__main__":
                 PredictAlgorithmFactory.generate_predictive_algorithm(partial(PredictiveRPBOnlineMinContinuousAlgorithm, pred_budget=4, max_support_factor=100), 'POPU'),
                 PredictAlgorithmFactory.generate_predictive_algorithm(partial(PredictiveRPBOnlineMinContinuousAlgorithm, pred_budget=8, max_support_factor=100), 'POPU'),
                 PredictAlgorithmFactory.generate_predictive_algorithm(partial(PredictiveRPBOnlineMinContinuousAlgorithm, pred_budget=16, max_support_factor=100), 'POPU'),
+                # HitCredit: persistent hit-accumulated credit (not reset on L0)
+                PredictAlgorithmFactory.generate_predictive_algorithm(partial(PredictiveRPBOnlineMinHitCreditAlgorithm, pred_budget=0, max_support_factor=100), 'POPU'),
+                PredictAlgorithmFactory.generate_predictive_algorithm(partial(PredictiveRPBOnlineMinHitCreditAlgorithm, pred_budget=1, max_support_factor=100), 'POPU'),
+                PredictAlgorithmFactory.generate_predictive_algorithm(partial(PredictiveRPBOnlineMinHitCreditAlgorithm, pred_budget=2, max_support_factor=100), 'POPU'),
+                PredictAlgorithmFactory.generate_predictive_algorithm(partial(PredictiveRPBOnlineMinHitCreditAlgorithm, pred_budget=4, max_support_factor=100), 'POPU'),
             ])
             combiner_types.extend([
                 (partial(CombineDeterministicAlgorithm, switch_bound=1, lazy_evictor_type=LRUEvictor), [PredictAlgorithmFactory.generate_predictive_algorithm(PredictAlgorithm, 'POPU'), LRUAlgorithm]),
